2219acfe69412794c8e6bbe92d1744ea259bc62e
mani5a3 <41730370+mani5a3@users.noreply.github.com>
2018-08-22 15:18:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,8 +6,8 @@ region ="us-east-2"
 }
 resource "aws_key_pair" "terraform_ec2_key" {
   key_name = "terraform_ec2_key"
-  public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmSldvm1i5VUjcoJsE/OIPGV1T577zwj6jP8+I+9n91Vy3+5ZJnBskLBSSh6NKtSOr1SPYQdKyND/hgrEd2mZThB7cPK+8EX7bUEC1h+BAGeQK/ZTkVjWnG1MkpJdPNaOaR8lnj/dVVPUhrJtKplCswSGX8CNdozbTfDJVHV7agJphdSLT968OAa+NvrOyivClvOoCFcbM/hZn8qiqNg4dKo95Ikg3E2CUnNgW8GfHfJ/UtW+dkniWPLmpCsKntkqEEFBXwQ6MYebWLbyLuhE31dWpokvppPixnIahYWVWB0S3Q7z7fcxhtVzTFmrfvtBDmnaZvpxmwy9O/VJbykux root@verinon-desktop"
-}
+  public_key="do ssh-keygen and paste here"
+  }
     resource "aws_instance" "terraformmachine"
 {
 
